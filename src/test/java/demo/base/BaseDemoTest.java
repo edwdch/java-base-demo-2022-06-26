d30b5e9a24666c66c1f577a1ac2316d62a4c4385
Edward Chen <hi@edch.top>
2022-06-26 23:49:45 +0800

    feat: add 2 test cases to test the "sum" method

--- src/test/java/demo/base/BaseDemoTest.java
+++ src/test/java/demo/base/BaseDemoTest.java
@@ -14,4 +14,22 @@ public class BaseDemoTest {
 
         assertEquals(expected, baseDemo.print());
     }
+
+    @Test
+    public void should_return_six() {
+        BaseDemo baseDemo = new BaseDemo();
+
+        int expected = 6;
+
+        assertEquals(expected, baseDemo.sum(3, 3));
+    }
+
+    @Test
+    public void should_return_ten() {
+        BaseDemo baseDemo = new BaseDemo();
+
+        int expected = 10;
+
+        assertEquals(expected, baseDemo.sum(5, 5));
+    }
 }
